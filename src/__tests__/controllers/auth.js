9432d70d1e7b7b073dd767255b40b0f4a9d54614
Víctor Ronda <victor.ronda@gmail.com>
2020-06-19 10:57:07 +0200

    starting testing the testing with axios-mock-adapter

--- src/__tests__/controllers/auth.js
+++ src/__tests__/controllers/auth.js
@@ -1,16 +1,17 @@
 import { isAuth } from '../../controllers/auth';
 import axios from 'axios';
 
+
 describe('Authorization tests depending on fullAccess',  () => {
     let mockData, mockCurrentUser, fullAccess;
-
     
-
+    
+    
     it('should return JSON data if the fullAccess === true and there is data',  () =>{
     fullAccess = true;
     mockData = { name: 'test' };
     mockCurrentUser = {role: 'admin'};
-    const res = axios.get.mockImplementation(() => Promise.resolve({ status: 200, data: mockData }));
+    const res = axios.get.mockImplementationOnce(() => Promise.resolve({ status: 200, data: mockData }));
     //console.log('Aquiiii', res);
     
     const result = isAuth(mockData,mockCurrentUser, fullAccess, res);
